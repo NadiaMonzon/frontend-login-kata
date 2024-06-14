9247de4590e37bd9b0716c93303aadb068d357b8
rochollbus <carlos.rocholl@innobus.g1844.es>
2024-06-14 19:24:31 +0100

    add: ErrorBoundary.tsx

---
+++ react-ts/src/components/ErrorBoundary.tsx
@@ -0,0 +1,39 @@
+import React, { ReactNode } from "react";
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: React.ErrorInfo) {
+    logErrorToMyService(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
+
+function logErrorToMyService(error: Error, info: React.ErrorInfo) {
+  console.log("Logging error to my service:", error, info);
+}
